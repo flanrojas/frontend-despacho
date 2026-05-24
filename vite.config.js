c34d51f8869039fc14472e8fdb2d8fe8ab19e21e
flanrojas <fra.rojasg@duocuc.cl>
2026-05-24 19:59:51 -0400

    Se agrega la url del backend

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://internal-balanceador-interno-843192543.us-east-1.elb.amazonaws.com/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
